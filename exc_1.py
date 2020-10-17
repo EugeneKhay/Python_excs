5870c8775250b131894666ab92d5bb46924928f6
Eugene <4358514@gmail.com>
2020-10-17 15:46:07 +0300

    ver. 2

--- exc_1.py
+++ exc_1.py
@@ -1,7 +1,8 @@
 import random
 
+available_attempts = 3
 num = random.randint(0, 100)
-attempts = 10
+attempts = available_attempts
 
 def user_input():
     res = 0
@@ -12,15 +13,30 @@ def user_input():
         print("Your input is wrong!")
         res = user_input()
     return res
+
+def play_again():
+    again = input("Play again (Y/N)?: ")
+    if again == "N":
+        return False
+    elif again == "Y":
+        global attempts
+        attempts = available_attempts
+        return True
+    else:
+        print("Unknown command")
+        return play_again()
     
 while True:
-    if attempts == 0:
+    if attempts <= 0:
         print("Too many attempts, you lose!")
+        if not play_again():
+            break
     s = user_input()
     attempts -= 1
     if s == num:
         print(f"Your number {s} is correct!")
-        break
+        if not play_again():
+            break   
     if s > num:
         print(f"Your number {s} is too big!")
     if s < num:
