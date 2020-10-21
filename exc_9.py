45578c7409079550b640dbac95593994969fd27f
Eugene <4358514@gmail.com>
2020-10-21 21:38:24 +0300

    exc 10

--- exc_9.py
+++ exc_9.py
@@ -25,8 +25,44 @@ def even_odd_sums(seq):
     return [sum(even), sum(odd)]
 
 
-def 
+def plus_minus(seq):
+    """ Return the result of alternately adding and subtracting numbers from each other
+        [10, 20, 30, 40, 50, 60])   10+20-30+40-50+60
+    """
+    items = even_odd_sums(seq)
+    res = items[0] - seq[0]*2
+    return items[1] - res
 
 
+def myzip(*iterables):
+    """ takes any number of iterables and returning a list of tuples. Each tuple
+        contains one element from each of the iterables passed to the function
+        Assume that all of the iterables are of the same length.
+    """
+    result_list = []
+    for i in range(len(iterables[0])):  
+        lst = []
+        for k in iterables:
+            lst.append(k[i])
+        result_list.append(tuple(lst))
+    return result_list
+
+
+def mysum(*args):
+    """ exercise 10
+    """
+    if not args:
+        return args
+    first = args[0]
+    # for i in range(1, len(args)):
+    #     first += args[i]
+    for i in args[1:]:
+        first += i
+    return first
+
+
+print(mysum())
+#print(myzip([10, 20,30], 'abc', (1, 2, 3)))
+#print(plus_minus([10, 20, 30, 40]))
 #print(first_last('shjdhjsd'))
-#print(even_odd_sums([10, 20, 30, 40, 50, 60]))
+#print(even_odd_sums([10, 20, 30, 40, 50, 60])
